f12f97db08edb44742ab8605bc229d8034f52453
Alexander Zhokhov <a.zhokhov@yandex.ru>
2023-03-28 13:27:49 +0300

    ci + no_working tests

---
+++ tests/unit_tests/test.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//#include "/home/azhok/CLionProjects/Project1/main.cpp"
+//#include <gtest/gtest.h>
+//TEST(Test, subtest) {
+//    std::ifstream f1("name.basics.tsv");
+//    Solution solution;
+//    std::string act = "Brigitte Bardot";
+//    std::vector<std::string> titles = {"tt0049189","tt0054452","tt0056404","tt0057345"};
+//    solution.actor = act;
+//    solution.Titles("name.basics.tsv");
+//    for(int i=0;i<titles.size();i++){
+//        ASSERT_NE(titles[i],solution.id_mov[i]);
+//    }
+//
+//}
+
