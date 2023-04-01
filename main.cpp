41532762c66981ea1089b9554551119babdf8ad0
Alexander Zhokhov <a.zhokhov@yandex.ru>
2023-04-01 16:27:38 +0300

    Rename variables in main cpp and add new gitignore

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 
 
-std::vector<std::string> Split(const std::string str, char sep) { //Функция, убирающая разделители и создающая вектор из частей строки
+std::vector<std::string>
+Split(const std::string str, char sep) { //Функция, убирающая разделители и создающая вектор из частей строки
     std::vector<std::string> result;
     std::string curr = " ";
     for (int i = 0; i < str.length(); i++) {
@@ -19,55 +20,51 @@ std::vector<std::string> Split(const std::string str, char sep) { //Функци
     return result;
 }
 
-class Solution {
+class NamesOfTitles {
 
 public:
-    std::vector <std::string> id_mov;
-    std::vector <std::string> non_adult_mov;
-    std::string actor;
-
-    void Titles(const std::string &f1)  //Находим в каких произведениях был актёр
+    void Titles(const std::string &file)  //Находим в каких произведениях был актёр
     {
-        std::ifstream input_file(f1);
+        std::ifstream inputFile(file);
         std::string actorInfo;
-        std::vector <std::string> listOfTitles;
-        while(getline(input_file, actorInfo)) {
-            if(actorInfo.find(actor, 0)!=-1) {
-                listOfTitles = Split(actorInfo,'\t');
-                id_mov = Split(listOfTitles[5],',');
-                id_mov[0] = id_mov[0].substr(1,'\n');
+        std::vector<std::string> Titles;
+        while (getline(inputFile, actorInfo)) {
+            if (actorInfo.find(actor) != -1) {
+                Titles = Split(actorInfo, '\t');
+                idMovie = Split(Titles[5], ',');
+                idMovie[0] = idMovie[0].substr(1, '\n');
             }
         }
-        input_file.close();
+        inputFile.close();
     }
 
-    void Movies(const std::string &f2)  //Смотрим фильмы не для взрослых этого актёра
+    void Movies(const std::string &file)  //Смотрим фильмы не для взрослых этого актёра
     {
         std::string titlesInfo;
         std::vector<std::string> movies;
         std::string title;
-        std::ifstream input_file(f2);
-        while (getline(input_file, titlesInfo)) {
-            for (int i = 0; i < id_mov.size(); i++) {
-                title = id_mov[i];
-                title = title.substr(1,'\n');
-                if (titlesInfo.find("movie", 0) != -1 && titlesInfo.find(title, 0) != -1) {
-                    movies = Split(titlesInfo,'\t');
-                    if(movies[4]==" 0") {
-                        non_adult_mov.push_back(title);
+        std::ifstream inputFile(file);
+        std::string movie = "movie";
+        while (getline(inputFile, titlesInfo)) {
+            for (int i = 0; i < idMovie.size(); i++) {
+                title = idMovie[i];
+                title = title.substr(1, '\n');
+                if (titlesInfo.find(movie) != -1 && titlesInfo.find(title) != -1) {
+                    movies = Split(titlesInfo, '\t');
+                    if (movies[4] == " 0") {
+                        nonAdultMovie.push_back(title);
                     }
                 }
             }
         }
-        input_file.close();
+        inputFile.close();
     }
 
-    std::string Local(const std::string &f3) //Смотрим есть ли у фильма русскоязычное название
+    std::string Local(const std::string &file) //Смотрим есть ли у фильма русскоязычное название
     {
         std::string nameInfo;
         std::string title;
         int k = 0;
-        int t = 0;
         std::vector<std::string> vectRU;
         std::vector<std::string> vectNoRU;
         std::vector<std::string> nameRU;
@@ -77,35 +74,35 @@ public:
         std::string titleNoRU;
         nameRU.reserve(8);
         name.reserve(8);
-        std::ifstream input_file(f3);
-        while (getline(input_file, nameInfo)) {
-            for (int i = 0; i < non_adult_mov.size(); i++) {
-                title = non_adult_mov[i];
-                if (nameInfo.find(title, 0) != -1 && (nameInfo.find("RU", 0) != -1 || nameInfo.find("SUHH", 0) != -1)) {
+        std::ifstream inputFile(file);
+        while (getline(inputFile, nameInfo)) {
+            for (int i = 0; i < nonAdultMovie.size(); i++) {
+                title = nonAdultMovie[i];
+                if (nameInfo.find(title) != -1 && (nameInfo.find("RU") != -1 || nameInfo.find("SUHH") != -1)) {
                     vectRU.push_back(title);
                 }
             }
         }
         std::sort(vectRU.begin(), vectRU.end());
         vectRU.erase(std::unique(vectRU.begin(), vectRU.end()), vectRU.end());
-        for (int i = 0; i < non_adult_mov.size(); i++) {
+        for (int i = 0; i < nonAdultMovie.size(); i++) {
             for (int j = 0; j < vectRU.size(); j++) {
-                if (non_adult_mov[i] != vectRU[j]) {
+                if (nonAdultMovie[i] != vectRU[j]) {
                     k++;
                 }
                 if (k == vectRU.size()) {
-                    vectNoRU.push_back(non_adult_mov[i]);
+                    vectNoRU.push_back(nonAdultMovie[i]);
                     k = 0;
                 }
             }
             k = 0;
         }
-        input_file.close();
-        input_file.open(f3);
-        while (getline(input_file, nameInfo)) {
+        inputFile.close();
+        inputFile.open(file);
+        while (getline(inputFile, nameInfo)) {
             for (int i = 0; i < vectRU.size(); i++) {
                 title = vectRU[i];
-                if (nameInfo.find(title, 0) != -1 && (nameInfo.find("RU", 0) != -1 || nameInfo.find("SUHH", 0) != -1)) {
+                if (nameInfo.find(title) != -1 && (nameInfo.find("RU") != -1 || nameInfo.find("SUHH") != -1)) {
                     nameRU = Split(nameInfo, '\t');
                     ListOfRU.push_back(nameRU[2]);
                 }
@@ -113,35 +110,38 @@ public:
             }
             for (int i = 0; i < vectNoRU.size(); i++) {
                 title = vectNoRU[i];
-                if (nameInfo.find(title, 0) != -1 && nameInfo.find("original", 0) != -1) {
+                if (nameInfo.find(title) != -1 && nameInfo.find("original") != -1) {
                     name = Split(nameInfo, '\t');
                     ListOfNoRU.push_back(name[2]);
                 }
             }
         }
-        input_file.close();
+        inputFile.close();
         std::sort(ListOfRU.begin(), ListOfRU.end());
         ListOfRU.erase(std::unique(ListOfRU.begin(), ListOfRU.end()), ListOfRU.end());
-        for(int i=0; i<ListOfRU.size();i++){
+        for (int i = 0; i < ListOfRU.size(); i++) {
             std::cout << ListOfRU[i] << std::endl;
         }
-        for(int i=0; i<ListOfNoRU.size();i++){
+        for (int i = 0; i < ListOfNoRU.size(); i++) {
             std::cout << ListOfNoRU[i] << std::endl;
         }
     }
+    std::vector<std::string> idMovie;
+    std::vector<std::string> nonAdultMovie;
+    std::string actor;
 };
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     if (argc != 5) {
         std::cerr << "Usage: " << argv[0] << " <name> <file1> <file2> <file3>" << std::endl;
         return 1;
     }
-    Solution solution;
+    NamesOfTitles namesOfTitles;
     std::string act = argv[1];
-    solution.actor = act;
+    namesOfTitles.actor = act;
 
-    solution.Titles(argv[2]);
-    solution.Movies(argv[3]);
-    solution.Local(argv[4]);
+    namesOfTitles.Titles(argv[2]);
+    namesOfTitles.Movies(argv[3]);
+    namesOfTitles.Local(argv[4]);
     return 0;
 }
--- tests/unit_tests/test.cpp
+++ tests/unit_tests/test.cpp
@@ -3,13 +3,13 @@
 //#include <gtest/gtest.h>
 //TEST(Test, subtest) {
 //    std::ifstream f1("name.basics.tsv");
-//    Solution solution;
+//    NamesOfTitles solution;
 //    std::string act = "Brigitte Bardot";
 //    std::vector<std::string> titles = {"tt0049189","tt0054452","tt0056404","tt0057345"};
 //    solution.actor = act;
 //    solution.Titles("name.basics.tsv");
 //    for(int i=0;i<titles.size();i++){
-//        ASSERT_NE(titles[i],solution.id_mov[i]);
+//        ASSERT_NE(titles[i],solution.idMovie[i]);
 //    }
 //
 //}
