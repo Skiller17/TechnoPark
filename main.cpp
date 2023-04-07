f9d38f4dbb5ac1891fda70f0e50598a1c98dbc5b
Alexander Zhokhov <a.zhokhov@yandex.ru>
2023-04-07 20:50:30 +0300

    reformatted methods Titles, Movies, Local

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <vector>
 
 
 std::vector<std::string>
-Split(const std::string str, char sep) { //Функция, убирающая разделители и создающая вектор из частей строки
-    std::vector<std::string> result;
+Split(const std::string &str, char separator) {
+    std::vector<std::string> result{};
     std::string curr = " ";
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] != sep) {
+
+    for (int i = 0; i < str.length(); ++i) {
+
+        if (str[i] != separator) {
             curr.push_back(str[i]);
         } else {
             result.push_back(curr);
@@ -21,127 +23,182 @@ Split(const std::string str, char sep) { //Функция, убирающая р
 }
 
 class NamesOfTitles {
-
 public:
-    void Titles(const std::string &file)  //Находим в каких произведениях был актёр
-    {
+    void InputInformationAboutActor(const std::string &file) {//Находим в каких произведениях был актёр
         std::ifstream inputFile(file);
-        std::string actorInfo;
-        std::vector<std::string> Titles;
-        while (getline(inputFile, actorInfo)) {
-            if (actorInfo.find(actor) != -1) {
-                Titles = Split(actorInfo, '\t');
-                idMovie = Split(Titles[5], ',');
-                idMovie[0] = idMovie[0].substr(1, '\n');
-            }
+        std::string informationAboutActor;
+
+        while (getline(inputFile, informationAboutActor)) {
+            SearchTitles(informationAboutActor);
         }
-        inputFile.close();
     }
 
-    void Movies(const std::string &file)  //Смотрим фильмы не для взрослых этого актёра
-    {
-        std::string titlesInfo;
-        std::vector<std::string> movies;
-        std::string title;
+    void SearchTitles(const std::string &informationAboutActor) {
+        std::vector<std::string> Titles{};
+        int firstTitle = 0;
+        int actorTitles = 5;
+
+        if (informationAboutActor.find(actor) != -1) {
+            Titles = Split(informationAboutActor, '\t');
+            idMovie = Split(Titles[actorTitles], ',');
+            idMovie[firstTitle] = idMovie[firstTitle].substr(1, '\n');
+        }
+    }
+
+    void InputInformationAboutTitles(const std::string &file) {
+        std::string informationAboutTitle;
         std::ifstream inputFile(file);
+
+        while (getline(inputFile, informationAboutTitle)) {
+            SearchNonAdultMovies(informationAboutTitle);
+        }
+    }
+
+    void SearchNonAdultMovies(const std::string &informationAboutTitle) {
+        std::string title;
         std::string movie = "movie";
-        while (getline(inputFile, titlesInfo)) {
-            for (int i = 0; i < idMovie.size(); i++) {
-                title = idMovie[i];
-                title = title.substr(1, '\n');
-                if (titlesInfo.find(movie) != -1 && titlesInfo.find(title) != -1) {
-                    movies = Split(titlesInfo, '\t');
-                    if (movies[4] == " 0") {
-                        nonAdultMovie.push_back(title);
-                    }
+        std::vector<std::string> movies{};
+        int isAdult = 4;
+
+        for (int i = 0; i < idMovie.size(); ++i) {
+            title = idMovie[i];
+            title = title.substr(1, '\n');
+
+            if (informationAboutTitle.find(movie) != -1 && informationAboutTitle.find(title) != -1) {
+                movies = Split(informationAboutTitle, '\t');
+
+                if (movies[isAdult] == " 0") {
+                    nonAdultMovie.push_back(title);
+                }
+            }
+        }
+    }
+
+    void SearchRuLocalizedTitles(const std::string &file) {//Смотрим есть ли у фильма русскоязычное название
+        std::string localizedTitles;
+        std::string title;
+        std::vector<std::string> ruLocalizedTitles{};
+        std::string titleNoRU;
+        std::ifstream inputFile(file);
+
+        while (getline(inputFile, localizedTitles)) {
+
+            for (int i = 0; i < nonAdultMovie.size(); ++i) {
+                title = nonAdultMovie[i];
+
+                if (localizedTitles.find(title) != -1 &&
+                    (localizedTitles.find("RU") != -1 || localizedTitles.find("SUHH") != -1)) {
+                    ruLocalizedTitles.push_back(title);
                 }
             }
         }
         inputFile.close();
+        SortRuTitlesAndSearchNoRuTitles(file, ruLocalizedTitles);
     }
 
-    std::string Local(const std::string &file) //Смотрим есть ли у фильма русскоязычное название
-    {
-        std::string nameInfo;
-        std::string title;
+    void SortRuTitlesAndSearchNoRuTitles(const std::string &file, std::vector<std::string> &ruLocalizedTitles) {
+        std::vector<std::string> noRuLocalizedTitles{};
         int k = 0;
-        std::vector<std::string> vectRU;
-        std::vector<std::string> vectNoRU;
-        std::vector<std::string> nameRU;
-        std::vector<std::string> name;
-        std::vector<std::string> ListOfRU;
-        std::vector<std::string> ListOfNoRU;
-        std::string titleNoRU;
-        nameRU.reserve(8);
-        name.reserve(8);
-        std::ifstream inputFile(file);
-        while (getline(inputFile, nameInfo)) {
-            for (int i = 0; i < nonAdultMovie.size(); i++) {
-                title = nonAdultMovie[i];
-                if (nameInfo.find(title) != -1 && (nameInfo.find("RU") != -1 || nameInfo.find("SUHH") != -1)) {
-                    vectRU.push_back(title);
-                }
-            }
-        }
-        std::sort(vectRU.begin(), vectRU.end());
-        vectRU.erase(std::unique(vectRU.begin(), vectRU.end()), vectRU.end());
+
+        std::sort(ruLocalizedTitles.begin(), ruLocalizedTitles.end());
+        ruLocalizedTitles.erase(std::unique(ruLocalizedTitles.begin(), ruLocalizedTitles.end()),
+                                ruLocalizedTitles.end());
+
         for (int i = 0; i < nonAdultMovie.size(); i++) {
-            for (int j = 0; j < vectRU.size(); j++) {
-                if (nonAdultMovie[i] != vectRU[j]) {
+
+            for (int j = 0; j < ruLocalizedTitles.size(); j++) {
+
+                if (nonAdultMovie[i] != ruLocalizedTitles[j]) {
                     k++;
                 }
-                if (k == vectRU.size()) {
-                    vectNoRU.push_back(nonAdultMovie[i]);
+
+                if (k == ruLocalizedTitles.size()) {
+                    noRuLocalizedTitles.push_back(nonAdultMovie[i]);
                     k = 0;
                 }
             }
             k = 0;
         }
-        inputFile.close();
-        inputFile.open(file);
-        while (getline(inputFile, nameInfo)) {
-            for (int i = 0; i < vectRU.size(); i++) {
-                title = vectRU[i];
-                if (nameInfo.find(title) != -1 && (nameInfo.find("RU") != -1 || nameInfo.find("SUHH") != -1)) {
-                    nameRU = Split(nameInfo, '\t');
-                    ListOfRU.push_back(nameRU[2]);
-                }
 
-            }
-            for (int i = 0; i < vectNoRU.size(); i++) {
-                title = vectNoRU[i];
-                if (nameInfo.find(title) != -1 && nameInfo.find("original") != -1) {
-                    name = Split(nameInfo, '\t');
-                    ListOfNoRU.push_back(name[2]);
-                }
-            }
+        InputLocalizedNames(file, ruLocalizedTitles, noRuLocalizedTitles);
+    }
+
+    void InputLocalizedNames(const std::string &file, std::vector<std::string> &ruLocalizedTitles, std::vector<std::string> &noRuLocalizedTitles) {
+        std::string localizedTitles;
+        std::ifstream inputFile(file);
+
+        while (getline(inputFile, localizedTitles)) {
+
+            SearchRuTitlesNames(localizedTitles, ruLocalizedTitles);
+            SearchNoRuTitlesNames(localizedTitles, noRuLocalizedTitles);
         }
-        inputFile.close();
-        std::sort(ListOfRU.begin(), ListOfRU.end());
-        ListOfRU.erase(std::unique(ListOfRU.begin(), ListOfRU.end()), ListOfRU.end());
-        for (int i = 0; i < ListOfRU.size(); i++) {
-            std::cout << ListOfRU[i] << std::endl;
-        }
-        for (int i = 0; i < ListOfNoRU.size(); i++) {
-            std::cout << ListOfNoRU[i] << std::endl;
+
+        OutputAndSortRuNames();
+        OutputNoRuNames();
+    }
+
+    void SearchRuTitlesNames(const std::string &localizedTitles, std::vector<std::string> &ruLocalizedTitles) {
+        std::vector<std::string> informationAboutRuLocalizedTitle{};
+        std::string title;
+
+        for (int i = 0; i < ruLocalizedTitles.size(); ++i) {
+            title = ruLocalizedTitles[i];
+
+            if (localizedTitles.find(title) != -1 &&
+                (localizedTitles.find("RU") != -1 || localizedTitles.find("SUHH") != -1)) {
+                informationAboutRuLocalizedTitle = Split(localizedTitles, '\t');
+                ruTitlesNames.push_back(informationAboutRuLocalizedTitle[2]);
+            }
         }
     }
-    std::vector<std::string> idMovie;
-    std::vector<std::string> nonAdultMovie;
+
+    void SearchNoRuTitlesNames(const std::string &localizedTitles, std::vector<std::string> &noRuLocalizedTitles) {
+        std::vector<std::string> informationAboutNoRuLocalizedTitles{};
+        std::string title;
+
+        for (int i = 0; i < noRuLocalizedTitles.size(); ++i) {
+            title = noRuLocalizedTitles[i];
+
+            if (localizedTitles.find(title) != -1 && localizedTitles.find("original") != -1) {
+                informationAboutNoRuLocalizedTitles = Split(localizedTitles, '\t');
+                noRuTitlesNames.push_back(informationAboutNoRuLocalizedTitles[2]);
+            }
+        }
+    }
+    void OutputAndSortRuNames() {
+        std::sort(ruTitlesNames.begin(), ruTitlesNames.end());
+        ruTitlesNames.erase(std::unique(ruTitlesNames.begin(), ruTitlesNames.end()), ruTitlesNames.end());
+
+        for (int i = 0; i < ruTitlesNames.size(); ++i) {
+            std::cout << ruTitlesNames[i] << std::endl;
+        }
+    }
+
+    void OutputNoRuNames() {
+        for (int i = 0; i < noRuTitlesNames.size(); ++i) {
+            std::cout << noRuTitlesNames[i] << std::endl;
+        }
+    }
+
     std::string actor;
+private:
+    std::vector<std::string> idMovie{};
+    std::vector<std::string> nonAdultMovie{};
+    std::vector<std::string> ruTitlesNames{};
+    std::vector<std::string> noRuTitlesNames{};
 };
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
-        std::cerr << "Usage: " << argv[0] << " <name> <file1> <file2> <file3>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <name> <file1> <file2> <file3>";
         return 1;
     }
     NamesOfTitles namesOfTitles;
     std::string act = argv[1];
     namesOfTitles.actor = act;
 
-    namesOfTitles.Titles(argv[2]);
-    namesOfTitles.Movies(argv[3]);
-    namesOfTitles.Local(argv[4]);
+    namesOfTitles.InputInformationAboutActor(argv[2]);
+    namesOfTitles.InputInformationAboutTitles(argv[3]);
+    namesOfTitles.SearchRuLocalizedTitles(argv[4]);
     return 0;
 }
--- tests/unit_tests/test.cpp
+++ tests/unit_tests/test.cpp
@@ -7,7 +7,7 @@
 //    std::string act = "Brigitte Bardot";
 //    std::vector<std::string> titles = {"tt0049189","tt0054452","tt0056404","tt0057345"};
 //    solution.actor = act;
-//    solution.Titles("name.basics.tsv");
+//    solution.SearchTitles("name.basics.tsv");
 //    for(int i=0;i<titles.size();i++){
 //        ASSERT_NE(titles[i],solution.idMovie[i]);
 //    }
